e37c81bee3a44080ab0c6d9c9dfcb8602c982467
Hartwin Hoyer <hlh@redhat.com>
2015-10-15 16:54:59 +0200

    login needed

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -21,7 +21,6 @@ class UserIdentity extends CUserIdentity
 			// username => password
 			'admin'=>'kickbaer',
 		);
-    return !$this->errorCode;
 		if(!isset($users[$this->username]))
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
 		elseif($users[$this->username]!==$this->password)
